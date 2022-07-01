6b993a5527404377d9fe328f64d42fa24d6480b2
Tomoya Sunda <sunda0707@icloud.com>
2022-07-01 20:31:48 +0900

    Use useStaticQuery hook

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,28 @@
 import "modern-css-reset";
+import { useStaticQuery, graphql } from "gatsby";
 
-const Layout = ({ children }) => children;
+const useLinks = () => {
+  const data = useStaticQuery(graphql`
+    {
+      r02 {
+        links {
+          id
+          name
+          url
+        }
+      }
+    }
+  `);
+
+  return data.r02.links;
+};
+
+const Layout = ({ children }) => {
+  const links = useLinks();
+
+  console.log(links);
+
+  return children;
+};
 
 export default Layout;
