1ce82f60c9c4618dacc16cba4463c519ee663f3d
dexn <sunda0707@icloud.com>
2022-07-09 00:02:04 +0900

    Create Header

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,15 @@ import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import { ThemeProvider } from "styled-components";
 import theme from "../theme";
-import { CssBaseline } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  CssBaseline,
+  Paper,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
 
 const useLinks = () => {
   const data = useStaticQuery(graphql`
@@ -20,6 +28,25 @@ const useLinks = () => {
   return data.r02.links;
 };
 
+const drawerWidth = 230;
+
+const StyledAppBar = styled(AppBar)`
+  width: calc(100% - ${drawerWidth}px);
+  margin-left: ${drawerWidth}px;
+`;
+
+const Header = () => {
+  return (
+    <StyledAppBar position="fixed">
+      <Toolbar>
+        <Typography component="h1" variant="h6">
+          Digiweb
+        </Typography>
+      </Toolbar>
+    </StyledAppBar>
+  );
+};
+
 const Layout = ({ children }) => {
   const links = useLinks();
 
@@ -28,7 +55,12 @@ const Layout = ({ children }) => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      {children}
+      <Box width={1} height={1}>
+        <Paper>
+          <Header />
+          {children}
+        </Paper>
+      </Box>
     </ThemeProvider>
   );
 };
